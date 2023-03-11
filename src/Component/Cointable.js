8e85d2a7a7846c8adc36bdd3d93721eafa76fe06
priyaghuguskar <ghuguskarpriya@gmail.com>
2023-03-11 23:32:26 +0530

    cointable add

--- src/Component/Cointable.js
+++ src/Component/Cointable.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { CoinList } from '../Config/Api';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import { CryptoState } from '../Context/CryptoContext';
-import { Container ,Form} from 'react-bootstrap';
+import { Container, Form, Table, Spinner, Pagination } from 'react-bootstrap';
+import { numberWithCommas } from './Banner/Slider';
+// import CryptoRow from './CoinRow';
 
 const Cointable = () => {
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState("");
+    const [page, setPage] = useState(1)
+    const history = useHistory()
 
-    const { currency } = CryptoState()
+    const { currency, symbol } = CryptoState()
 
     const fetchCoins = async () => {
         setLoading(true)
@@ -18,27 +24,130 @@ const Cointable = () => {
     };
     console.log(coins)
     useEffect(() => {
-fetchCoins()
+        fetchCoins()
     }, [currency])
+
+    const handlesearch = () => {
+       
+        return coins.filter(
+            (coin) =>
+                coin.name.toLowerCase().includes(search) ||
+                coin.symbol.toLowerCase().includes(search)
+        )
+    }
+
 
 
     return (
         <>
-        <Container>
-            <div className='cointable-head'>
-                Cryptocurrency Prices by Market Cap
-            </div>
-            <Form className="" variant='dark'>
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-        </Container>
+            <Container>
+                <div className='cointable-head'>
+                    Cryptocurrency Prices by Market Cap
+                </div>
+                <Form className='form-serach' variant="dark" bg="dark">
+                    <input type="text"
+                        placeholder='Search for Crypto Currency...'
+                        className="coin-search"
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                </Form>
+                {loading ? (
+                    <Spinner animation="border" />
+                ) : (
+                    <Table>
+                        <thead>
+                            <tr className='Table-head-row'>
+                                {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                                    <th key={head}
+                                    >{head}</th>
+                                ))}
+
+                            </tr>
+                        </thead>
+
+                        <tbody className='Table-data-row'>
+                            {handlesearch().map((row) => {
+                                let profit = row.price_change_percentage_24h > 0;
+                                return (
+                                    <tr className='table-row' onClick={() => history.push(`/coins/${row.id}`)}
+                                        key={row.name}
+                                    >
+                                        <td style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            alignContent: "center",
+                                            justifyContent: "left",
+                                            // flexDirection:"column",
+                                            gap: 15,
+                                        }}>
+                                            <img
+                                                src={row.image}
+                                                alt={row.name}
+                                                height="50px"
+                                                // width="50px"
+                                                style={{ marginBottom: 10 }}
+                                            />
+                                            <div style={{ display: "flex", flexDirection: "column" }}
+                                            >
+                                                <span
+                                                    style={{
+                                                        textTransform: "uppercase",
+                                                        fontSize: "20"
+                                                    }}
+                                                > {row.symbol}
+                                                </span>
+                                                <span
+                                                    style={{ color: "darkgrey" }}>{row.name}
+                                                </span>
+
+                                            </div>
+                                        </td>
+                                        <td align='center'>
+                                            {symbol}{" "}
+                                            {numberWithCommas(row.current_price.toFixed(2))}
+                                        </td>
+                                        <td
+                                            align='center'
+                                            style={{
+                                                color: profit > 0 ? "rgb(14,203,129)" : "red",
+                                                fontWeight: 500,
+                                            }}>
+                                            {profit && "+"}
+                                            {row.price_change_percentage_24h.toFixed(2)}%
+
+                                        </td>
+                                        <td align='center'>
+                                            {symbol}{" "}
+                                            {numberWithCommas(
+                                                row.market_cap.toString().slice(0, -6)
+                                            )}
+                                            M
+                                        </td>
+
+                                    </tr>)
+
+                            })}
+
+
+
+
+
+                        </tbody>
+
+
+                    </Table>
+
+                )}
+      
+
+            </Container>
         </>
     )
 }
 
 export default Cointable
+
+{/* //   ) : (
+    //     <Spinner animation="border" />
+    //   )} */}
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom'
+import CoinInfo from '../Component/CoinInfo';
+import { SingleCoin } from '../Config/Api';
+// import { CryptoState } from '../Context/CryptoContext';
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  // const { currency, symbol } = CryptoState()
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data)
+  }
+  console.log(coin)
+  useEffect(() => {
+    fetchCoin();
+  },)
+
+
+
   return (
-    <div>CoinPage</div>
+    <Container style={{ width: "100%", margin: "10px" }}>
+      <div className='coin-info'>
+        <div className='coin-side'>
+
+          <img
+            src={coin?.image.large}
+            alt={coin?.name}
+            height="200px"
+            style={{ marginBottom: "8px" }}
+          />
+          <div style={{
+            fontWeight: 'bold',
+            marginBottom: "10px",
+            fontSize: "1.1em"
+          }}>{coin?.name}
+          </div>
+          <div className='coin-des'>
+            {(coin?.description.en.slice(0, 200))}
+          </div>
+          <div classname='coin-marketdata'>
+            <div>
+              <h3>Rank: </h3><span> {coin?.market_cap_rank}</span>
+              </div>
+          </div>
+        </div>
+        <div><CoinInfo coin={coin} /></div>
+      </div>
+
+
+
+    </Container>
   )
 }
 
