89690d74aed7c819f13aabb7b96f1fae9b423e7d
priyaghuguskar <ghuguskarpriya@gmail.com>
2023-05-14 18:15:07 +0530

    crypto context

--- src/Component/Cointable.js
+++ src/Component/Cointable.js
@@ -7,6 +7,7 @@ import { Container, Form, Table, Spinner} from 'react-bootstrap';
 import { numberWithCommas } from './Banner/Slider';
 // import CryptoRow from './CoinRow';
 
+
 const Cointable = () => {
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(false);
